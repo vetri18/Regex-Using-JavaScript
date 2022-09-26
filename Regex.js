dd7bdb4e036edd571b9653019cd31d8155943d0a
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-26 13:10:19 +0530

    Create a regex pattern to validate PIN Code

--- Regex.js
+++ Regex.js
@@ -1,1 +1,5 @@
 console.log("Wellcome to the Regex problem using javascript");
+
+let sampleRegex = RegExp('^[1-9]{1}[0-9]{5}$');
+let sampleCheck = sampleRegex.test("400088");
+console.log(sampleCheck);
