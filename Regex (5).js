8dc6884dcc246f5a7604d4de6a64194422f7a8d9
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-26 13:05:32 +0530

    close the expression with optional parts

---
+++ Regex (5).js
@@ -0,0 +1,12 @@
+console.log("Wellcome to the Regex problem using javascript");
+
+let sampleRegex = RegExp('^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$');
+let sampleCheck = sampleRegex.test("400 088");
+console.log(sampleCheck);
+
+let EmailRegex = RegExp('^[a-zA-Z]{3,}([_,+,-,.][a-zA-Z0-9]+)*[@]{1}[a-zA-Z]{10,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$');
+let EmailCheck = EmailRegex.test("abc.xyz@bridgelabz.co.in");
+console.log(EmailCheck);
+
+
+
